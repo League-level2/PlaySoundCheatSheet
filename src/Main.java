2d847d01d432d9d1470a9c8641036fd2b936e621
sdvic <gvw@san.rr.com>
2017-09-03 20:23:44 -0700

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,29 @@
+import javax.swing.*;
+import java.applet.AudioClip;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Main
+{
+    URL sound;
+    AudioClip noise;
+
+    public static void main(String[] args)
+    {
+        Main main = new Main();
+        main.getGoing();
+    }
+
+    private void getGoing()
+    {
+        try
+        {
+            sound = new URL("file:/Users/VicMini/git/PlaySoundCheatSheet/src/bell.aiff") ;
+            noise = JApplet.newAudioClip(sound);
+            noise.play();
+        } catch (MalformedURLException e)
+        {
+            e.printStackTrace();
+        }
+    }
+}
